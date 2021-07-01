f01122b48e6ac36748a38f6f1c34f2a5c5f110dd
Alvaro Carrascosa <varocarras@gmail.com>
2021-07-01 19:11:55 -0400

    DEMO

---
+++ Models/train.py
@@ -0,0 +1,137 @@
+import physionet_utilities as pc
+import ecg_plot
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow_addons as tfa
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import network
+
+batchsize = 30
+epochN = 100
+
+checkpoint_path = "Models/resnet.hdf5"
+
+
+gender, age, labels, ecg_filenames = pc.import_key_data("Data/")
+ecg_filenames = np.asarray(ecg_filenames)
+
+import os
+signal_lenght=[]
+for subdir, dirs, files in sorted(os.walk("Data/")):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+        if filepath.endswith(".mat"):
+            data, header_data = pc.load_challenge_data(filepath)
+            splitted = header_data[0].split()
+            signal_lenght.append(splitted[3])
+signal_lenght_df = pd.DataFrame(signal_lenght)
+signal_count=signal_lenght_df[0].value_counts()
+
+age, gender = pc.import_gender_and_age(age, gender)
+
+
+SNOMED_scored=pd.read_csv("Data/mappings/SNOMED_mappings_scored.csv", sep=";")
+SNOMED_unscored=pd.read_csv("Data/mappings/SNOMED_mappings_unscored.csv", sep=";")
+df_labels = pc.make_undefined_class(labels,SNOMED_unscored)
+
+
+y , snomed_classes = pc.onehot_encode(df_labels)
+
+
+snomed_abbr = []
+for j in range(len(snomed_classes)):
+    for i in range(len(SNOMED_scored.iloc[:,1])):
+        if (str(SNOMED_scored.iloc[:,1][i]) == snomed_classes[j]):
+            snomed_abbr.append(SNOMED_scored.iloc[:,2][i])
+            
+snomed_abbr = np.asarray(snomed_abbr)
+
+y_all_comb = pc.get_labels_for_all_combinations(y)
+folds = pc.split_data(labels, y_all_comb)
+order_array = folds[0][0]
+
+new_weights=pc.calculating_class_weights(y)
+
+keys = np.arange(0,27,1)
+weight_dictionary = dict(zip(keys, new_weights.T[1]))
+weight_dictionary
+
+reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(
+    monitor='val_AUC', factor=0.1, patience=1, verbose=1, mode='max',
+    min_delta=0.0001, cooldown=0, min_lr=0
+)
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_AUC', mode='max', verbose=1, patience=2)
+
+from scipy import optimize
+
+def thr_chall_metrics(thr, label, output_prob):
+    return -pc.compute_challenge_metric_for_opt(label, np.array(output_prob>thr))
+
+
+model = RESNET()
+
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+model.fit(x=shuffle_batch_generator(batch_size=batchsize, gen_x=generate_X_shuffle(ecg_filenames), gen_y=generate_y_shuffle(y)), epochs=epochN,verbose=1, steps_per_epoch=(len(order_array)/batchsize), validation_data=pc.generate_validation_data(ecg_filenames,y,folds[0][1]), validation_freq=1, class_weight=weight_dictionary, callbacks=[reduce_lr,early_stop])
+model.save_weights(checkpoint_path)
+
+def shuffle_batch_generator_demo(batch_size, gen_x,gen_y, gen_z): 
+    np.random.shuffle(order_array)
+    batch_features = np.zeros((batch_size,5000, 12))
+    batch_labels = np.zeros((batch_size,snomed_classes.shape[0])) #drop undef class
+    batch_demo_data = np.zeros((batch_size,2))
+    while True:
+        for i in range(batch_size):
+
+            batch_features[i] = next(gen_x)
+            batch_labels[i] = next(gen_y)
+            batch_demo_data[i] = next(gen_z)
+
+        X_combined = [batch_features, batch_demo_data]
+        yield X_combined, batch_labels
+        
+def shuffle_batch_generator(batch_size, gen_x,gen_y): 
+    np.random.shuffle(order_array)
+    batch_features = np.zeros((batch_size,5000, 12))
+    batch_labels = np.zeros((batch_size,snomed_classes.shape[0])) #drop undef class
+    while True:
+        for i in range(batch_size):
+
+            batch_features[i] = next(gen_x)
+            batch_labels[i] = next(gen_y)
+            
+        yield batch_features, batch_labels
+
+def generate_y_shuffle(y_train):
+    while True:
+        for i in order_array:
+            y_shuffled = y_train[i]
+            yield y_shuffled
+
+
+def generate_X_shuffle(X_train):
+    while True:
+        for i in order_array:
+                #if filepath.endswith(".mat"):
+                    data, header_data = pc.load_challenge_data(X_train[i])
+                    X_train_new = pad_sequences(data, maxlen=5000, truncating='post',padding="post")
+                    X_train_new = X_train_new.reshape(5000,12)
+                    yield X_train_new
+
+def generate_z_shuffle(age_train, gender_train):
+    while True:
+        for i in order_array:
+            gen_age = age_train[i]
+            gen_gender = gender_train[i]
+            z_train = [gen_age , gen_gender]
+            yield z_train
